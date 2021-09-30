a89df1080cd8e9bf27d53a1d48f09c82d4bfc952
raaraya1 <83246056+raaraya1@users.noreply.github.com>
2021-09-30 10:28:41 -0300

    Add files via upload

---
+++ Mapas.py
@@ -0,0 +1,59 @@
+import openrouteservice
+import folium
+import numpy as np
+
+class Mapas():
+  def __init__(self, key, lugares):
+    self.key = key
+    self.client = openrouteservice.Client(key=key)
+    self.matrix_de_distancias = [] 
+    self.lugares = lugares
+    
+
+  def matriz_de_distancias(self, unidad='km'):
+    matrix_de_distancias = self.client.distance_matrix(locations=self.lugares, metrics = ("duration", "distance"), units = unidad)
+    return matrix_de_distancias['distances']
+
+  def Mapa_con_lugares(self):
+    coordenadas_folium = [[i[1], i[0]] for i in self.lugares]
+    punto_central = np.mean(coordenadas_folium, axis=0)
+    map = folium.Map(location=punto_central, zoom_start=15)
+
+    for indx, coord in enumerate(coordenadas_folium):
+      folium.Marker(coord, popup=f'P{indx+1}').add_to(map)
+
+    return map
+
+  def Mapa_con_rutas(self, rutas_especificas=0):
+    map = self.Mapa_con_lugares()
+
+    if rutas_especificas == 0:
+      rutas = []
+      for i in self.lugares:
+        for j in self.lugares:
+          if i != j:
+            input = [i, j]
+            rutas.append(self.client.directions(coordinates=input, profile='driving-car', format='geojson'))
+
+      dic_routes = {}
+      for i in range(len(rutas)):
+        dic_routes[f'ruta_{i+1}'] = rutas[i]
+
+      for i in dic_routes:
+        folium.GeoJson(dic_routes[i], name=i).add_to(map)
+
+      folium.LayerControl().add_to(map)
+
+    elif rutas_especificas != 0:
+      dic_routes = {}
+      for i in rutas_especificas:
+        input = [rutas_especificas[i][0], rutas_especificas[i][1]]
+        ruta = self.client.directions(coordinates=input, profile='driving-car', format='geojson')
+        dic_routes[i] = ruta
+      
+      for i in dic_routes:
+        folium.GeoJson(dic_routes[i], name=i).add_to(map)
+
+      folium.LayerControl().add_to(map)
+
+    return map
